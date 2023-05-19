7df7d0ff66f1b73f0b07466082c84f5b02f56c49
Igor Czyż <igorczyz13@gmail.com>
2023-05-19 09:39:01 +0200

    plugin sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,61 @@
-const {src, dest} = require('gulp');
+const {
+    src,
+    dest,
+    series
+} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cssnano = require('gulp-cssnano');
 const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify')
+const imagemin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
+
+
+const paths = {
+    sass: './src/sass/**/*.scss',
+    js: './src/js/**/*.js',
+    img: './src/img/*',
+    sassDest: './dist/css',
+    jsDest: './dist/js',
+    imgDest: './dist/img'
+}
 
 function sassCompiler(done) {
-    src('./src/sass/**/*.scss')
+    src(paths.sass)
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(cssnano())
-        .pipe(dest('./dist/css'));
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(dest(paths.sassDest));
     done()
 }
 
-exports.default = sassCompiler
+function javaScript(done) {
+    src(paths.js)
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(dest(paths.jsDest));
+    done()
+}
+
+function convertImages(done) {
+    src(paths.img)
+        .pipe(imagemin())
+        .pipe(dest(paths.imgDest));
+    done()
+}
+
+exports.default = series(sassCompiler, javaScript, convertImages)
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+const test = () => {
+    console.log('Test');
+    console.log('Drugi test');
+    // komentarz
+    console.log(test2);
+}
+
+test()
